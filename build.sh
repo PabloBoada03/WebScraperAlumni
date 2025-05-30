b89e133c0a896bac963ac34f941a4bc84c4b7f03
PaBoad03 <pabloboaro@unisabana.edu.co>
2025-05-30 12:16:56 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
-# Instala Chromium y ChromeDriver
-apt-get update && apt-get install -y chromium chromium-driver
+# Instala Chrome y su driver
+apt-get update && apt-get install -y wget gnupg unzip
+wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | apt-key add -
+echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google-chrome.list
+apt-get update && apt-get install -y google-chrome-stable
 
-# Instala dependencias de Python
+# Instala ChromeDriver que sea compatible con Chrome
+CHROME_VERSION=$(google-chrome-stable --version | grep -oP "\d+\.\d+\.\d+")
+CHROMEDRIVER_VERSION=$(wget -qO- "https://chromedriver.storage.googleapis.com/LATEST_RELEASE_$CHROME_VERSION")
+wget -O /tmp/chromedriver.zip "https://chromedriver.storage.googleapis.com/${CHROMEDRIVER_VERSION}/chromedriver_linux64.zip"
+unzip /tmp/chromedriver.zip -d /usr/local/bin/
+chmod +x /usr/local/bin/chromedriver
+
+# Instala dependencias Python
 pip install -r requirements.txt
